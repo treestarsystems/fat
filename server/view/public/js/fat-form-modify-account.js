c58d8064cc49fe53cadee5c46933c2250ffb8f43
Tree Star Systems <info@treestarsystems.com>
2021-09-27 23:25:00 -0400

    front: fix edit submission, back: add edit endpoint and fix update query

--- server/view/public/js/fat-form-modify-account.js
+++ server/view/public/js/fat-form-modify-account.js
@@ -301,7 +301,6 @@ async function addAccountPrompt () {
 }
 
 function generateAccountItemsHTML (arrayOfAccountObjs) {
-console.log(arrayOfAccountObjs)
  let returnObj = {"status": "","message": "","payload": ""};
  try {
   if (!isNonEmptyArray(arrayOfAccountObjs)) throw 'Invalid Array';
@@ -401,7 +400,6 @@ async function submitAccounts(containerDivID,type,accountUUID) {
   }
 //An API endpoint needs to be added for this functionality.
   if (type == 'edit') {
-console.log(returnObj.payload)
    axios({
     method: 'put',
     url: 'api/edit/account',
